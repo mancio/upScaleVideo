cfe01e84f98b60d5df7f2d12f16a16ab47b81c1c
Andrea Mancini <mancioman2@gmail.com>
2023-11-03 16:20:27 +0100

    first

---
+++ main.py
@@ -0,0 +1,73 @@
+import os
+import sys
+import subprocess
+
+one_k = ["1280", "720p"]
+two_k = ["2048", "2K"]
+four_k = ["3840", "4K"]
+
+very_sharp = "unsharp=7:7:2.0:5:5:1.0"
+sharp = "unsharp=5:5:0.8:3:3:0.4"
+
+
+def upscale_video_to_4k(input_path, output_path):
+    # command = [
+    #     'ffmpeg',
+    #     '-i', input_path,        # Input file
+    #     "-vf", "scale=3840:-1",  # Scaling
+    #     "-vcodec", "h264_nvenc",
+    #     output_path,             # Output file
+    #     "-y"
+    # ]
+
+    command = [
+        'ffmpeg',
+        # '-hwaccel', 'cuda',                # Enable CUDA hardware acceleration
+        '-i', input_path,  # Input file path
+        '-vf', f'scale={one_k[0]}:-2:flags=lanczos,'  # Use NPP scaling with Lanczos.
+               f'gblur=sigma=1.0',  # increase blur effect
+        # f'{very_sharp}',  # -2 maintains aspect ratio
+        '-c:v', 'h264_nvenc',  # Specify Nvidia's hardware-accelerated H.264 video codec
+        '-preset', 'slow',  # Preset for the encoder speed/quality tradeoff
+        '-profile:v', 'high',  # Specify H.264 profile level
+        '-pix_fmt', 'yuv420p',  # Pixel format for compatibility
+        '-rc', 'vbr',  # Rate control method: Variable Bit Rate
+        '-cq', '19',  # Constant Quality for VBR mode
+        '-qmin', '18',  # Minimum quantizer scale
+        '-qmax', '23',  # Maximum quantizer scale
+        '-maxrate', '50M',  # Maximum bitrate
+        '-bufsize', '100M',  # Buffer size
+        '-acodec', 'copy',  # Copy audio stream without re-encoding
+        output_path,  # Output file path
+        '-y',  # Overwrite output file without asking
+        '-loglevel', 'error',  # This will make ffmpeg not verbose
+    ]
+
+    # Print the entire command as a single string
+    full_command = ' '.join(command)
+    print("Executing command:", full_command)
+
+    subprocess.run(command)
+
+
+def process_directory(directory):
+    # Create an "upscaled" subdirectory
+    output_directory = os.path.join(directory, "upscaled")
+    os.makedirs(output_directory, exist_ok=True)
+
+    # Loop through all files in the directory
+    for filename in os.listdir(directory):
+        if filename.endswith(".mp4"):
+            input_path = os.path.join(directory, filename)
+            output_path = os.path.join(output_directory, f"{os.path.splitext(filename)[0]}-{one_k[1]}.mp4")
+            print(f"Processing {filename}...")
+            upscale_video_to_4k(input_path, output_path)
+            print(f"Saved upscaled video to {output_path}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script_name.py <directory_path>")
+        sys.exit(1)
+
+    process_directory(sys.argv[1])
